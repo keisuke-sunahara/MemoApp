a6ee707eac8b596270fb4f0a9914e46ae0837106
砂原圭輔 <suna@sunaharakeisukenoiMac.local>
2021-10-17 13:23:12 +0900

    Extract components

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function AppBar() {
+  return (
+    <View style={styles.appbar}>
+      <View style={styles.appbarInner}>
+        <Text style={styles.appberTitle}>Memo App</Text>
+        <Text style={styles.appbarRight}>ログアウト</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appbar: {
+    width: '100%',
+    height: 104,
+    backgroundColor: '#51CF8B',
+    justifyContent: 'flex-end',
+  },
+  appbarInner: {
+    alignItems: 'center',
+  },
+  appbarRight: {
+    position: 'absolute',
+    right: 19,
+    bottom: 12,
+    color: 'rgba(155, 255, 255, 0.8)',
+  },
+  appberTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+});
