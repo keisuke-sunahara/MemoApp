bc1238a4e6ffee33d0387c328b9c1bc3da416acf
砂原圭輔 <suna@sunaharakeisukenoiMac.local>
2021-10-18 22:43:03 +0900

    Create MempoDetailScreen

--- src/components/CircleButton.jsx
+++ src/components/CircleButton.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { string, shape } from 'prop-types';
 
 export default function CircleButton(props) {
-  const { children } = props;
+  const { children, style } = props;
   return (
-    <View style={styles.circleButton}>
+    <View style={[styles.circleButton, style]}>
       <Text style={styles.circleButtomLabel}>{children}</Text>
     </View>
   );
 }
+
+CircleButton.propTypes = {
+  children: string.isRequired,
+  style: shape(),
+};
+CircleButton.defaultProps = {
+  style: null,
+};
 
 const styles = StyleSheet.create({
   circleButton: {
