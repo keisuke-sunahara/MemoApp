a89c48b65b47bde5061648b9756b75ab0597863a
砂原圭輔 <suna@sunaharakeisukenoiMac.local>
2021-10-28 23:50:25 +0900

    Create Lading component

--- src/screens/LogInScreen.jsx
+++ src/screens/LogInScreen.jsx
@@ -5,11 +5,13 @@ import {
 import firebase from 'firebase';
 
 import Button from '../components/Button';
+import Loading from '../components/Loading';
 
 export default function LogInScreen(props) {
   const { navigation } = props;
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const unsubcribe = firebase.auth().onAuthStateChanged((user) => {
@@ -18,11 +20,14 @@ export default function LogInScreen(props) {
           index: 0,
           routes: [{ name: 'MemoList' }],
         });
+      } else {
+        setLoading(false);
       }
     });
     return unsubcribe;
   }, []);
   const handlePress = useCallback(() => {
+    setLoading(true);
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then((userCredential) => {
         const { user } = userCredential;
@@ -34,10 +39,14 @@ export default function LogInScreen(props) {
       })
       .catch((error) => {
         Alert.alert(error.code);
+      })
+      .then(() => {
+        setLoading(false);
       });
   }, [email, password]);
   return (
     <View style={styles.container}>
+      <Loading isLoading={isLoading} />
       <View style={styles.inner}>
         <Text style={styles.title}>Log In</Text>
         <TextInput
