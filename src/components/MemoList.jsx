6c48b2f1ef6e904a6b0f188dd319fcdbf310c751
砂原圭輔 <suna@sunaharakeisukenoiMac.local>
2021-10-31 19:04:04 +0900

    Minor fix

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -42,7 +42,7 @@ export default function MemoList(props) {
         style={styles.memoListItem}
         onPress={() => { navigation.navigate('MemoDetail', { id: item.id }); }}
       >
-        <View>
+        <View style={styles.memoInner}>
           <Text style={styles.memoListItemTitle} numberOfLines={1}>{item.bodyText}</Text>
           <Text style={styles.memoListItemDate}>{dateToString(item.updateAt)}</Text>
         </View>
@@ -100,5 +100,8 @@ const styles = StyleSheet.create({
   },
   memoDelete: {
     padding: 8,
+  },
+  memoInner: {
+    flex: 1,
   },
 });
