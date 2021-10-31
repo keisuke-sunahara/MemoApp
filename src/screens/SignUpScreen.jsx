5d80021ed3339231c8d3f43f86f67f786cfd5bc7
砂原圭輔 <suna@sunaharakeisukenoiMac.local>
2021-10-31 18:25:09 +0900

    Improve ux

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -5,6 +5,7 @@ import {
 import firebase from 'firebase';
 
 import Button from '../components/Button';
+import { translateErrors } from '../utils';
 
 export default function SignUpScreen(props) {
   const { navigation } = props;
@@ -22,8 +23,9 @@ export default function SignUpScreen(props) {
         });
       })
       .catch((error) => {
+        const errorMsg = translateErrors(error.code);
         console.log(error.code, error.message);
-        Alert.alert(error.code);
+        Alert.alert(errorMsg.title, errorMsg.description);
       });
   }, [email, password]);
 
